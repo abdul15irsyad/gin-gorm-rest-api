ce7f1b5fa3876d38d034559502a297cca0d902db
Irsyad Abdul <abdulirsyad15@gmail.com>
2024-11-15 13:31:13 +0700

    refactor: create provide multiple

--- main.go
+++ main.go
@@ -2,12 +2,18 @@ package main
 
 import (
 	"fmt"
+	"gin-gorm-rest-api/config"
+	"gin-gorm-rest-api/controllers"
+	"gin-gorm-rest-api/middlewares"
+	"gin-gorm-rest-api/routes"
+	"gin-gorm-rest-api/services"
 	"log"
 	"os"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	"go.uber.org/dig"
 )
 
 func main() {
@@ -31,10 +37,67 @@ func main() {
 	router.SetTrustedProxies([]string{})
 	router.Static("/assets", "./assets")
 
-	InitDependencies(router)
+	// init routes
+	allRoutes := InitRoutes(router)
+	for _, route := range allRoutes {
+		route.Init(router)
+	}
 
 	// listen on port
 	Port := os.Getenv("PORT")
 	log.Println("environment=" + Env)
 	router.Run("localhost:" + fmt.Sprint(Port))
+}
+
+type Route interface {
+	Init(r *gin.Engine)
+}
+
+func ProvideMultiple(container *dig.Container, constructors []interface{}) error {
+	for _, constructor := range constructors {
+		if err := container.Provide(constructor); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func InitRoutes(router *gin.Engine) []Route {
+	container := dig.New()
+	if err := ProvideMultiple(container, []interface{}{
+		// config
+		config.NewDatabaseConfig,
+		// middlewares
+		middlewares.NewAuthMiddleware,
+		// services
+		services.NewFileService,
+		services.NewJwtService,
+		services.NewRoleService,
+		services.NewUserService,
+		// controllers
+		controllers.NewAuthUserController,
+		controllers.NewAuthController,
+		controllers.NewFileController,
+		controllers.NewRoleController,
+		controllers.NewRootController,
+		controllers.NewUserController,
+		// routes
+		routes.NewAuthRoute,
+		routes.NewFileRoute,
+		routes.NewRoleRoute,
+		routes.NewRootRoute,
+		routes.NewUserRoute,
+	}); err != nil {
+		panic(err)
+	}
+
+	allRoutes := []Route{}
+
+	if err := container.Invoke(func(authRoute *routes.AuthRoute, fileRoute *routes.FileRoute, roleRoute *routes.RoleRoute, rootRoute *routes.RootRoute, userRoute *routes.UserRoute) {
+		allRoutes = []Route{authRoute, fileRoute, roleRoute, rootRoute, userRoute}
+	}); err != nil {
+		panic(err)
+	}
+
+	return allRoutes
 }
